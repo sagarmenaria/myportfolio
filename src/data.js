1169c59a89081e56722c30df93a6866f10e3badf
sagarmenaria <sagarmenaria00@gmail.com>
2024-03-31 02:05:40 +0530

    first commit

---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "React And Redux",
+    subtitle: "MERN Stack",
+    description:
+      "E-commerce Website which is used to buy day to day products with smooth UI experience",
+    image: "./fashionstore.png",
+    link: "https://sagarfashionstore.netlify.app",
+  },
+  {
+    title: "Wordpress",
+    subtitle: "Elementor And Astra",
+    description:
+      "For Webdevelopment Company To make versetile project using cutting edge Technologies ",
+    image: "./WebDevelpment.png",
+    link: "https://sagarmenaria.liveblog365.com",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Epic Todo App",
+    subtitle: "React Hooks",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-4.gif",
+    link: "https://reactbootcamp.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Vinay Yadav",
+    company: "vstechplus",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ashib Tharu",
+    company: "Fashion Store",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "MongoDB",
+  "Wordpress",
+];
